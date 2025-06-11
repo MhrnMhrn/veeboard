769ae44eb403f7020f36b951b315627ffdacf6ae
Mehran Goudarzi <skullboxmhrn@gmail.com>
2025-06-11 12:30:11 +0330

    using default system sounds instead of external file

--- veeboard.m
+++ veeboard.m
@@ -30,7 +30,7 @@ static NSString *Elide(NSString *s) {
 @interface VeeboardApp : NSObject <NSApplicationDelegate>
 @property (nonatomic, strong) NSMutableArray<NSString*> *history;
 @property (nonatomic, strong) NSPanel *panel;
-@property (nonatomic, strong) NSSound *clipSound; // Added property declaration
+@property (nonatomic, strong) NSSound *clipSound;
 - (void)addClipboardText:(NSString*)t;
 - (void)togglePanel;
 @end
@@ -38,19 +38,20 @@ static NSString *Elide(NSString *s) {
 @implementation VeeboardApp
 
 - (instancetype)init {
-    self = [super init];
-    if (self) {
-        NSString *soundPath = [[NSBundle mainBundle] pathForResource:@"tick" ofType:@"wav"];
-        if (soundPath) {
-            self.clipSound = [[NSSound alloc] initWithContentsOfFile:soundPath byReference:YES];
+    if ((self = [super init])) {
+        /*
+        / Use macOS built-in sounds (you can change this to any of the system sounds you want)
+        here is the list:
+        Basso, Blow, Bottle, Frog, Funk, Glass, Hero, Morse, Ping, Pop, Purr, Sosumi, Submarine, Tink
+        */
+        self.clipSound = [NSSound soundNamed:@"Frog"];
+        if (self.clipSound)
             [self.clipSound setVolume:0.5];
-        } else {
-            NSLog(@"WARNING: tick.wav not found in main bundle");
-        }
+        else
+            NSLog(@"WARNING: system sound “Frog” not available");
     }
     return self;
 }
-
 
 - (void)rebuildPanel {
     if (!self.panel) {
@@ -65,7 +66,6 @@ static NSString *Elide(NSString *s) {
         self.panel.hidesOnDeactivate = NO;
         self.panel.movableByWindowBackground = YES;
     }
-
     NSView *content = self.panel.contentView;
     [content.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
 
@@ -83,26 +83,25 @@ static NSString *Elide(NSString *s) {
         btn.action = @selector(buttonClicked:);
         btn.toolTip = snippet;
 
-        // Create an icon for "⌘N"
         NSString *cmdText = [NSString stringWithFormat:@"⌘%ld", idx + 1];
         NSImage *icon = [[NSImage alloc] initWithSize:NSMakeSize(24, 24)];
         [icon lockFocus];
-        NSDictionary *attributes = @{
+        NSDictionary *attrs = @{
             NSFontAttributeName: [NSFont systemFontOfSize:14],
             NSForegroundColorAttributeName: [NSColor blackColor]
         };
-        NSSize textSize = [cmdText sizeWithAttributes:attributes];
-        [cmdText drawAtPoint:NSMakePoint((24 - textSize.width) / 2, (24 - textSize.height) / 2)
-              withAttributes:attributes];
+        NSSize ts = [cmdText sizeWithAttributes:attrs];
+        [cmdText drawAtPoint:NSMakePoint((24 - ts.width)/2, (24 - ts.height)/2)
+             withAttributes:attrs];
         [icon unlockFocus];
 
         btn.image = icon;
-        btn.imagePosition = NSImageLeft; // Icon on the left, title on the right
+        btn.imagePosition = NSImageLeft;
         btn.imageScaling = NSImageScaleNone;
-
         [content addSubview:btn];
     }];
 }
+
 - (void)buttonClicked:(NSButton*)sender {
     NSUInteger idx = (NSUInteger)sender.tag;
     NSString *text = self.history[idx];
@@ -113,52 +112,43 @@ static NSString *Elide(NSString *s) {
     [self.history removeObjectAtIndex:idx];
     [self.history insertObject:text atIndex:0];
     [self.panel orderOut:nil];
-    // Simulate Command+V to paste the content
+
     dispatch_async(dispatch_get_main_queue(), ^{
-        // Create and post Command+V key down event
-        CGEventSourceRef source = CGEventSourceCreate(kCGEventSourceStateHIDSystemState);
-        CGEventRef cmdVDown = CGEventCreateKeyboardEvent(source, kVK_ANSI_V, true);
-        CGEventSetFlags(cmdVDown, kCGEventFlagMaskCommand);
-        CGEventPost(kCGSessionEventTap, cmdVDown);
+        CGEventSourceRef src = CGEventSourceCreate(kCGEventSourceStateHIDSystemState);
+        CGEventRef down = CGEventCreateKeyboardEvent(src, kVK_ANSI_V, true);
+        CGEventSetFlags(down, kCGEventFlagMaskCommand);
+        CGEventPost(kCGSessionEventTap, down);
 
-        // Post key up event
-        CGEventRef cmdVUp = CGEventCreateKeyboardEvent(source, kVK_ANSI_V, false);
-        CGEventSetFlags(cmdVUp, kCGEventFlagMaskCommand);
-        CGEventPost(kCGSessionEventTap, cmdVUp);
+        CGEventRef up = CGEventCreateKeyboardEvent(src, kVK_ANSI_V, false);
+        CGEventSetFlags(up, kCGEventFlagMaskCommand);
+        CGEventPost(kCGSessionEventTap, up);
 
-        // Clean up
-        CFRelease(cmdVDown);
-        CFRelease(cmdVUp);
-        CFRelease(source);
+        CFRelease(down);
+        CFRelease(up);
+        CFRelease(src);
     });
 }
 
 - (void)addClipboardText:(NSString *)t {
-    if (self.clipSound) {
-            [self.clipSound play];
-        }
+    if (self.clipSound) [self.clipSound play];
     if (!t.length) return;
     if (self.history.count && [self.history[0] isEqualToString:t]) return;
     [self.history insertObject:t atIndex:0];
     if (self.history.count > kHistoryMax)
         [self.history removeLastObject];
-    
 }
 
 - (void)togglePanel {
     [self rebuildPanel];
-    if (self.panel.isVisible) {
+    if (self.panel.isVisible)
         [self.panel orderOut:nil];
-    } else {
-        //[NSApp activateIgnoringOtherApps:YES];
+    else {
         [self.panel center];
         [self.panel orderFront:nil];
     }
 }
 
 @end
-
-// ───────── Event tap callbacks ──────────────────────────────────────────────
 
 static CGEventRef copyTap(CGEventTapProxy proxy,
                           CGEventType type,
@@ -169,9 +159,10 @@ static CGEventRef copyTap(CGEventTapProxy proxy,
     if (flags & kCGEventFlagMaskCommand) {
         UniChar c; UniCharCount n;
         CGEventKeyboardGetUnicodeString(event, 1, &n, &c);
-        if (n == 1 && (c == 'c' || c == 'x')) {
-            VeeboardApp *app = (__bridge VeeboardApp *)refcon;
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        if (n == 1 && (c=='c' || c=='x')) {
+            VeeboardApp *app = (__bridge VeeboardApp*)refcon;
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1*NSEC_PER_SEC)),
+                           dispatch_get_main_queue(), ^{
                 NSString *s = [[NSPasteboard generalPasteboard]
                                stringForType:NSPasteboardTypeString];
                 [app addClipboardText:s];
@@ -187,64 +178,49 @@ static CGEventRef hotTap(CGEventTapProxy proxy,
                          void *refcon)
 {
     if (type != kCGEventKeyDown) return event;
-
     CGEventFlags flags = CGEventGetFlags(event);
-    VeeboardApp *app = (__bridge VeeboardApp *)refcon;
+    VeeboardApp *app = (__bridge VeeboardApp*)refcon;
 
-    // Handle Command+Shift+V to toggle panel
-    if ((flags & (kCGEventFlagMaskCommand | kCGEventFlagMaskShift)) == (kCGEventFlagMaskCommand | kCGEventFlagMaskShift)) {
+    // Command+Shift+V
+    if ((flags & (kCGEventFlagMaskCommand|kCGEventFlagMaskShift)) == (kCGEventFlagMaskCommand|kCGEventFlagMaskShift)) {
         CGKeyCode kc = (CGKeyCode)CGEventGetIntegerValueField(event, kCGKeyboardEventKeycode);
         if (kc == kVK_ANSI_V) {
             dispatch_async(dispatch_get_main_queue(), ^{
                 [app togglePanel];
             });
-            return NULL; // Consume the event
+            return NULL;
         }
     }
-
-    // Handle Command+<number> when panel is visible
-    if ((flags & kCGEventFlagMaskCommand) == kCGEventFlagMaskCommand && app.panel.isVisible) {
+    // Cmd+number when panel visible
+    if ((flags & kCGEventFlagMaskCommand)==kCGEventFlagMaskCommand && app.panel.isVisible) {
         CGKeyCode kc = (CGKeyCode)CGEventGetIntegerValueField(event, kCGKeyboardEventKeycode);
         NSUInteger idx = NSNotFound;
-
-        // Map number keys (0-9) to indices
         switch (kc) {
-            case kVK_ANSI_1: idx = 0; break;
-            case kVK_ANSI_2: idx = 1; break;
-            case kVK_ANSI_3: idx = 2; break;
-            case kVK_ANSI_4: idx = 3; break;
-            case kVK_ANSI_5: idx = 4; break;
-            case kVK_ANSI_6: idx = 5; break;
-            case kVK_ANSI_7: idx = 6; break;
-            case kVK_ANSI_8: idx = 7; break;
-            case kVK_ANSI_9: idx = 8; break;
-            case kVK_ANSI_0: idx = 9; break;
+            case kVK_ANSI_1: idx=0; break; case kVK_ANSI_2: idx=1; break;
+            case kVK_ANSI_3: idx=2; break; case kVK_ANSI_4: idx=3; break;
+            case kVK_ANSI_5: idx=4; break; case kVK_ANSI_6: idx=5; break;
+            case kVK_ANSI_7: idx=6; break; case kVK_ANSI_8: idx=7; break;
+            case kVK_ANSI_9: idx=8; break; case kVK_ANSI_0: idx=9; break;
             default: break;
         }
-
-        if (idx != NSNotFound && idx < app.history.count) {
-            // Simulate button click
-            NSButton *button = [[NSButton alloc] init];
-            button.tag = (NSInteger)idx;
+        if (idx!=NSNotFound && idx<app.history.count) {
+            NSButton *btn = [NSButton new];
+            btn.tag = (NSInteger)idx;
             dispatch_async(dispatch_get_main_queue(), ^{
-                [app buttonClicked:button];
+                [app buttonClicked:btn];
             });
-            return NULL; // Consume the event
+            return NULL;
         }
     }
-
     return event;
 }
-
-// ─────────────── main ────────────────────────────────────────────────────────
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         if (!AXIsProcessTrusted()) {
-            NSDictionary *opts = @{(__bridge id)kAXTrustedCheckOptionPrompt: @YES};
+            NSDictionary *opts = @{(__bridge id)kAXTrustedCheckOptionPrompt:@YES};
             AXIsProcessTrustedWithOptions((__bridge CFDictionaryRef)opts);
         }
-
         [NSApplication sharedApplication];
         VeeboardApp *delegate = [[VeeboardApp alloc] init];
         delegate.history = [NSMutableArray array];
@@ -253,41 +229,29 @@ int main(int argc, const char * argv[]) {
 
         CGEventMask mask = CGEventMaskBit(kCGEventKeyDown);
 
-        // Cmd-C / Cmd-X
         CFMachPortRef tap1 = CGEventTapCreate(
             kCGSessionEventTap, kCGHeadInsertEventTap,
             kCGEventTapOptionDefault, mask,
-            copyTap, (__bridge void *)delegate
+            copyTap, (__bridge void*)delegate
         );
-        if (!tap1) {
-            NSLog(@"ERROR: cannot tap Cmd-C/Cmd-X; check Accessibility");
-            return 1;
-        }
-        CFRunLoopSourceRef src1 = CFMachPortCreateRunLoopSource(
-            kCFAllocatorDefault, tap1, 0
-        );
+        if (!tap1) { NSLog(@"ERROR: cannot tap Cmd-C/Cmd-X"); return 1; }
+        CFRunLoopSourceRef src1 = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, tap1, 0);
         CFRunLoopAddSource(CFRunLoopGetMain(), src1, kCFRunLoopCommonModes);
         CFRelease(src1);
         CFRelease(tap1);
 
-        // Command+Shift+V
         CFMachPortRef tap2 = CGEventTapCreate(
             kCGSessionEventTap, kCGHeadInsertEventTap,
             kCGEventTapOptionDefault, mask,
-            hotTap, (__bridge void *)delegate
+            hotTap, (__bridge void*)delegate
         );
-        if (!tap2) {
-            NSLog(@"ERROR: cannot tap Command-Shift-V; check Accessibility");
-            return 1;
-        }
-        CFRunLoopSourceRef src2 = CFMachPortCreateRunLoopSource(
-            kCFAllocatorDefault, tap2, 0
-        );
+        if (!tap2) { NSLog(@"ERROR: cannot tap Cmd-Shift-V"); return 1; }
+        CFRunLoopSourceRef src2 = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, tap2, 0);
         CFRunLoopAddSource(CFRunLoopGetMain(), src2, kCFRunLoopCommonModes);
         CFRelease(src2);
         CFRelease(tap2);
 
-        NSLog(@"Veeboard started. Use ⌘C/⌘X to record, Command+Shift+V to show.");
+        NSLog(@"Veeboard started. Use ⌘C/⌘X to record, ⌘⇧V to show.");
         [NSApp run];
     }
     return 0;
